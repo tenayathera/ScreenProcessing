f6ba5ec6255bc4ccb32e2a1ca609686c0cb0ed66
apfejes <afejes@tenayathera.com>
2018-04-11 11:53:47 -0700

    Add a unit tests - demonstrates there's an existing bug in the code.

---
+++ tests/test_fastgq_to_counts.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+
+from fastqgz_to_counts import parse_library_fasta
+
+
+class TestFastqgzToCounts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.fasta_file_test = '../library_reference/CRISPRi_v2_human.trim_1_29_forward.fa'
+
+    def test_find_start_no_max_exon_len(self):
+        print(os.getcwd())
+        seq_to_id_dict, ids_to_readcount_dict, expected_read_length = parse_library_fasta(self.fasta_file_test)
+
+        # first element:
+        self.assertIn('GAGACCCAGCGCTAACCAGGTTTAAGAG', seq_to_id_dict)
+        self.assertEqual(seq_to_id_dict['GAGACCCAGCGCTAACCAGGTTTAAGAG'], ['A1BG_-_58858617.23-P1'])
+
+        self.assertEqual(len(seq_to_id_dict), 205647)
+        self.assertEqual(expected_read_length, 28)
+
+        self.assertEqual(len(ids_to_readcount_dict), 209069)
+
+        # last element:
+        self.assertIn('AGAAGCCAACCTCGCGTTAGTTTAAGAG', seq_to_id_dict)
+        self.assertEqual(seq_to_id_dict['AGAAGCCAACCTCGCGTTAGTTTAAGAG'], ['non-targeting_03789'])
